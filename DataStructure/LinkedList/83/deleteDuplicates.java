dcbd68304bea20820084818267dd4f0b0547085d
Yuxin Song <yuxin.song@yunzhanghu.com>
2020-06-22 14:41:16 +0800

    6.22

--- DataStructure/LinkedList/83/deleteDuplicates.java
+++ DataStructure/LinkedList/83/deleteDuplicates.java
@@ -1,1 +1,36 @@
-qq
+83. Remove Duplicates from Sorted List
+        Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+        Example 1:
+
+        Input: 1->1->2
+        Output: 1->2
+        Example 2:
+
+        Input: 1->1->2->3->3
+        Output: 1->2->3
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+1. iterate directly
+// just change the link between two same nodes
+class Solution {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+            return head;
+        }
+    }
