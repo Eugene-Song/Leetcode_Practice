6486b116ddfbf1da66dce9a80c9f69f37f2d5909
Yuxin Song <yuxinsong@192.168.1.97>
2020-06-26 12:13:07 +0800

    6.26

---
+++ DataStructure/LinkedList/206/reverseList.java
@@ -0,0 +1,29 @@
+206. Reverse Linked List
+        Reverse a singly linked list.
+
+        Example:
+
+        Input: 1->2->3->4->5->NULL
+        Output: 5->4->3->2->1->NULL
+        Follow up:
+
+        A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// recursion
+class Solution {
+        public ListNode reverseList (ListNode head){
+            if (head == null || head.next == null) return head;
+            ListNode p = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return p;
+        }
+}
